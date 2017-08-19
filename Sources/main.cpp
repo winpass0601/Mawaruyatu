e3603b500c53bf9c74f22cbcec5be1e7f7f26842
Keisuke　Honda <nkc.keisuke.honda@gmail.com>
2017-08-20 01:25:51 +0900

    [update]デバイス作成失敗時のメッセージ追加

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -10,11 +10,15 @@ int  __stdcall WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCm
 	HF::Window::GetInstance()->Create("†Mawaruyatu†", SCREEN_X, SCREEN_Y);
 
 	//デバイス作成
-	HF::Direct3D::Instance().InitDirect3D(
+	HRESULT hr;
+	if (FAILED(hr = HF::Direct3D::Instance().InitDirect3D(
 		HF::Window::GetInstance()->GetHandle(),
 		HF::Window::GetInstance()->m_ScreenWidth,
 		HF::Window::GetInstance()->m_ScreenHeight
-		);
+		)))
+	{
+		throw(Exception(hr, _T("Direct3Dの初期化に失敗しました。")));
+	}
 
 	while (HF::Window::MessageHandling())
 	{
